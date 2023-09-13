71cf14a0adf9404f44d83f4c8248c08d34771e6a
Marcotmn <129113184+Marcotmn@users.noreply.github.com>
2023-09-13 12:35:57 +0200

    risolto un bug

--- capstone.codify/src/main/java/marco/tumminia/capstone/codify/entities/utente/UtenteService.java
+++ capstone.codify/src/main/java/marco/tumminia/capstone/codify/entities/utente/UtenteService.java
@@ -2,6 +2,7 @@ package marco.tumminia.capstone.codify.entities.utente;
 
 import java.util.List;
 
+import marco.tumminia.capstone.codify.exceptions.BadRequestException;
 import marco.tumminia.capstone.codify.exceptions.IncorrectPasswordException;
 
 import java.util.UUID;
@@ -11,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
+import  marco.tumminia.capstone.codify.entities.utente.NuovoUtentePayload;
 import marco.tumminia.capstone.codify.entities.azienda.Azienda;
 import marco.tumminia.capstone.codify.entities.privato.Privato;
 import marco.tumminia.capstone.codify.entities.sviluppatore.Sviluppatore;
@@ -23,15 +24,14 @@ public class UtenteService {
     @Autowired
     private UtenteRepository utenteRepository;
     
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    
-
-    public Utente save(NuovoUtentePayload payload) {
-        Utente utente = new Utente();
-
-        return utenteRepository.save(utente);
-    }
+	// SALVA NUOVO UTENTE + ECCEZIONE SE VIENE USATA LA STESSA EMAIL
+	public Utente save(NuovoUtentePayload body) {
+		utenteRepository.findByEmail(body.getEmail()).ifPresent(utente -> {
+			throw new BadRequestException("L'email " + body.getEmail() + " è gia stata utilizzata");
+		});
+		Utente newUtente = new Utente(body.getUsername(), body.getEmail(),body.getPassword(), body.getIndirizzo(), body.getNumeroTelefono(), body.getCartaDiCredito(), body.getRuolo());
+		return utenteRepository.save(newUtente);
+	}
 
     
     public Utente findByEmail(String email) {
